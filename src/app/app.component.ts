0b965c6ccf6b0e170b8b1b3b293086aa5ad38849
Mehjbeen123 <tambolisami4@gmail.com>
2024-02-04 06:44:14 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'reactive';
+  public myForm:any;
+  constructor(private fb:FormBuilder){
+    this.myForm = this.fb.group({
+      fname:[''],
+      lname:[''],
+      email:[''],
+      pass:[''],
+      dob:[''],
+      gender:[''],
+      street:[''],
+      con:[''],
+      city:[''],
+      region:[''],
+      postal:['']
+
+    })
+  }
+  onSubmit(){
+    console.log(this.myForm)
+
+  }
+  get f(){
+    return this.myForm.controls;
+
+  }
+}
